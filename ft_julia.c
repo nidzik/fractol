13616aabac1ac98dc500eaaacb7cf3717e79eecd
nidzik <nidzik@student.42.fr>
2015-05-02 08:54:21 +0200

    add julia

--- ft_julia.c
+++ ft_julia.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_fractol.c                                       :+:      :+:    :+:   */
+/*   ft_julia.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: nidzik <nidzik@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2015/05/01 16:58:08 by nidzik            #+#    #+#             */
-/*   Updated: 2015/05/01 17:48:11 by nidzik           ###   ########.fr       */
+/*   Created: 2015/05/01 16:59:46 by nidzik            #+#    #+#             */
+/*   Updated: 2015/05/01 18:35:26 by nidzik           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,12 +17,12 @@ t_env	ft_init_env(t_benv be)
 	t_env e;
 
 	/* printf("factor = %f\n", be.factor);fflush(stdout); */
-	e.x1 = -2.1 + be.factorx1;// - be.factor;
-	e.x2 = 0.6 - be.factorx2;// + be.factor;
-	e.y1 = -1.2 + be.factory1;// - be.factor;
-	e.y2 = 1.3 - be.factory2;//+ be.factor;
-	e.zoom = (be.factor);
-	e.ite_max = 300; 
+	e.x1 = -1 ;
+	e.x2 = 1 ;
+	e.y1 = -1.2 ;
+	e.y2 = 1.2;
+	e.zoom =  (be.factor);
+	e.ite_max = 128; 
 	e.image_x = 800;//(e.x2 - e.x1) * e.zoom;
 	e.image_y = 800;// (e.y2 - e.y1) * e.zoom;
 	/* printf("%f, %f, %f, %f, %d, %d, %d, %d \n ",e.x1, e.x2, e.y1, e.y2, e.zoom, e.ite_max, e.image_x, e.image_y); */
@@ -41,12 +41,12 @@ void ft_set_pixel(t_benv *be)
 		e.y = 0;
 		while (e.y < e.image_y)
 		{
-			e.c_r = 2 * (e.x - 800 / 2) /
-				(0.5 * be->factor * 800) + be->movex;
-			e.c_i = 2 * (e.y - 800 / 2) /
-				(0.5 * be->factor * 800) + be->movey;
-			e.z_r = 0;
-			e.z_i = 0;
+			e.c_r = -0.6 + be->factorx1;
+			e.c_i = 0.27015 + be->factorx2;
+
+
+			e.z_r = 1.5 * (e.x - 400) / (0.5 * e.zoom * 800 )+ be->movex;
+			e.z_i = (e.y - 400) / (0.5 * e.zoom * 800 )+ be->movey;
 			e.i = 0;
 			while ((e.z_r * e.z_r + e.z_i * e.z_i < 4 && e.i <
 							e.ite_max) || e.i == 0)
@@ -56,9 +56,18 @@ void ft_set_pixel(t_benv *be)
 				e.z_i = 2 * e.z_i * e.tmp + e.c_i;
 				e.i += 1;
 			}
+			if (e.i < e.ite_max)
+			{
+				be->data[(800 * e.y + e.x) * 4 + 1] = 0*(50 + e.i * 1.5);
+				be->data[(800 * e.y + e.x) * 4 + 2] = 0*(50 + e.i * 1.5);
+				be->data[(800 * e.y + e.x) * 4 + 3] = 0*(50 + e.i * 1.5);
+			}
+			else 
+			{
 				be->data[(800 * e.y + e.x) * 4 + 1] = (50 + e.i * 1.5);
-				be->data[(800 * e.y + e.x) * 4 + 2] = (50 + e.i * 1.5);
+				be->data[(800 * e.y + e.x) * 4 + 2] = 0*(50 + e.i * 1.5);
 				be->data[(800 * e.y + e.x) * 4 + 3] = (50 + e.i * 1.5);
+			}
 			e.y += 1;
 		}
 		e.x += 1;
--- ft_main.c
+++ ft_main.c
@@ -3,12 +3,14 @@
 
 int	mouse_hook(int button, int x, int y, t_benv *be)
 {
+	
+
 	/* printf("mouse : %d (%d:%d)\n",button, x, y); */
 	if (button == 4)
 	{
 		be->factor *= 1.1;
-	be->movex += ((double)x - (800 / 2)) * 0.00038 /
-		be->factor;
+		be->movex += ((double)x - (800 / 2)) * (0.00038 /
+					be->factor);
 	/* printf("movex / %f", be->movex); */
 	be->movey += ((double)y - (800 / 2)) * 0.00038 /
 		be->factor;
@@ -31,40 +33,55 @@ int	key_hook(int keycode, t_benv *be)
 	/* printf("key : %d \n",keycode);fflush(stdout); */
 	if (keycode == 65307)
 		exit(0);
-	if (keycode == 65362)
+	if (keycode == 'q')
+		be->factorx1 += 0.1;
+	if (keycode == 's')
+		be->factorx2 += 0.01;
+	if (keycode == 'w')
+		be->factorx1 += 0.1;
+	if (keycode == 'x')
+		be->factorx2 += 0.01;
+	if (keycode == 'a')
+		be->movey -= 0.1 / be->factor;
+	if (keycode == 'z')
+		be->movex -= 0.1 / be->factor;
+	if (keycode == 'e')
+		be->movey += 0.1 / be->factor;
+	if (keycode == 'r')
+		be->movex += 0.1 / be->factor;
+	if (keycode == 't')
 		be->factor *= 1.1;
-	if (keycode == 65364)
+	if (keycode == 'y')
 		be->factor /= 1.1;
-	if (keycode == 97)
-		be->factorx1+=1;
-	if (keycode == 122)
-		be->factorx2+=1;
-	if (keycode == 101)
-		be->factory1+=1;
-	if (keycode == 114)
-		be->factory2+=1;
-	if (keycode == 113)
-		be->factorx1-=1;
-	if (keycode == 115)
-		be->factorx2-=1;
-	if (keycode == 100)
-		be->factory1-=1;
-	if (keycode == 102)
-		be->factory2-=1;
 	expose_hook(be);
 	return (0);
+}
+int loop_hook(t_benv *be)
+{
+	be->movex += 0.1;
+	printf("hey");fflush(stdout);
+	ft_set_pixel(be);
+	mlx_clear_window(be->mlx, be->win);
+	mlx_put_image_to_window(be->mlx, be->win, be->img, 0, 0 );
+
+	return (0);
+	
 }
 
 int coucou(t_benv be)
 {
+
 	ft_set_pixel(&be);
 //	mlx_clear_window(be.mlx, be.win);
+
 	mlx_put_image_to_window(be.mlx, be.win, be.img, 0, 0 );
 	return (0);
 }
 
 int	expose_hook(t_benv *be)
 {
+	mlx_loop_hook(be->win, loop_hook, be);
+	//loop_hook(be);
 	coucou(*be);
 	return (0);
 }
@@ -89,6 +106,8 @@ int	main(int ac, char **av)
 	mlx_key_hook(be.win, key_hook, &be);
 	mlx_mouse_hook(be.win, mouse_hook, &be);
 	mlx_expose_hook(be.win, expose_hook, &be);
+
 	mlx_loop(be.mlx);
+	/* mlx_loop_hook(be.win, loop_hook, &be); */
 	return (0);
 }
